010f1e7ebaed259f4d82dd6ad420ec72303a30b0
Tanvir Ahammed <mdtanvirahammed3070@gmail.com>
2020-10-15 22:08:46 +0600

    first commit

---
+++ home.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<body>
+<h2>HTML Forms</h2>
+<form action="loginaction/result.php" method='post'>
+  <label for="fname">First name:</label><br>
+  <input type="text" id="fname" name="fname" value="Tanvir"><br>
+  <label for="lname">Last name:</label><br>
+  <input type="text" id="lname" name="lname" value="Ahammed"><br><br>
+  <input type="submit" value="Submit">
+</form> 
+
+
+</body>
+</html>
---
+++ loginaction/result.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<body>
+<h1>Registration Successful</h1>
+
+
+
+</body>
+</html>
